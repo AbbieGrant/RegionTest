02ae96a14abf02483e096154c83dd78658c12dc4
i7674769 <i7674769@mac27775.local>
2015-10-26 12:09:25 +0000

    Initial Commit

---
+++ RegionTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RegionTest
+//
+//  Created by Rebecca Hughes (i7674769) on 26/10/2015.
+//  Copyright © 2015 Rebecca Hughes (i7674769). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
