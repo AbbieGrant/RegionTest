975e31940cfe37ea3305f48134fc210e7bc38c80
AbbieGrant <i7643664@mac27770.local>
2015-11-23 13:36:18 +0000

    basic

--- RegionTest/ViewController.swift
+++ RegionTest/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMap: MKMapView!
+    
+    
+    let locationManager = CLLocationManager ()
+
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        myMap.setUserTrackingMode(.Follow, animated: true)
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        
+        
+        let bournemouthPier = CLLocationCoordinate2D (latitude: 50.715992, longitude: -1.876398)
+        let bournemouthPierRegion = CLCircularRegion (center: bournemouthPier, radius: 100, identifier: "bournemouth")
+        locationManager.startMonitoringForRegion(bournemouthPierRegion)
+        
+        
+        
+        let boscombePier = CLLocationCoordinate2D (latitude: 50.715992, longitude: -1.876398)
+        let boscombePierRegion = CLCircularRegion (center: boscombePier, radius: 100, identifier: "boscombe")
+        locationManager.startMonitoringForRegion(boscombePierRegion)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+ 
 
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+   
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entering \(region.identifier)")
+       
+        image.image = UIImage(named: region.identifier)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+         print("Exiting \(region.identifier)")
+    }
+
+}
